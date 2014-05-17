a484acb25a52962b6bf14a9630ea00b887076019
Kyle Kravette <kkravette@gmail.com>
2014-05-17 18:42:00 -0400

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+LocaShort::Application.config.secret_key_base = '21093436c700842b097c2565348189adb2ce392950b2aa50a2866ebf979fd160dc075a108b603282affb2e672a1dd1f08f7a94802631112e8cc4dcc76e2ab010'
