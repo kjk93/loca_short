90bd7b6e0e1da650ec849c35cf117800ffa9c056
Kyle Kravette <kkravette@gmail.com>
2014-05-22 00:41:48 -0400

    hash method first test

--- app/controllers/shorten_controller.rb
+++ app/controllers/shorten_controller.rb
@@ -3,6 +3,7 @@ class ShortenController < ApplicationController
   end
 
   def create
+  	hash = hash()
   	@short = Short.new
   end
 
@@ -11,4 +12,38 @@ class ShortenController < ApplicationController
 
   def error
   end
+
+  private 
+  	def hash()
+  		@short = Short.last
+  		return inc(@short.short)
+  	end
+
+  	def inc(hash) #increment hash value
+  		lib = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+  		alib = lib.each_char.to_a
+
+		ahash = hash.each_char.to_a #string put into array
+
+		len = ahash.length
+
+		if ahash[len - 1] == "z"
+			i = 1 #counter for end of string
+
+			while ahash[len - i] == "z" do
+				ahash[len - i] = "0"
+				i = i + 1 # next character
+			end
+
+			return ahash.join
+		else
+			original = ahash[len - 1]
+
+			inc = alib[lib.index(original) + 1] # incremented value characrter
+
+			ahash[len - 1] = inc
+
+			return ahash.join
+		end
+  	end
 end
